a591eafbdf021ee0faa1b7493ab9ea7dc5e32d8d
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-03-24 00:54:09 +0330

    create and define file and folder

---
+++ bubble-contact.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Plugin Name:       bubble contact
+ * Plugin URI:        https://github.com/farzane-na/bubble-contact-plugin
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            farzane nazmabadi
+ * Author URI:        https://farzanenazmabadi.ir
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       bubble-contact
+ * Domain Path:       /languages
+ * Requires Plugins:  my-plugin, yet-another-plugin
+ */
+
+
+ define(BUBBLE_CONTACT_FRONT_STYLE,plugin_dir_url( __FILE__ )."asset/css/app.css");
+ define(BUBBLE_CONTACT_FRONT_SCRIPT,plugin_dir_url( __FILE__ )."asset/js/app.js");
+ define(BUBBLE_CONTACT_ADMIN_VIEW,plugin_dir_url( __FILE__ )."view/admin-view.php");
+ define(BUBBLE_CONTACT_ADMIN_FILE,plugin_dir_url( __FILE__ )."include/admin.php");
